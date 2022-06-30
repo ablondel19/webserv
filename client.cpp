5c9c06ca597e1a87f8a84846bec66ddb4b1c67f4
ablondel19 <ablondel@student.s19.be>
2022-06-30 16:19:38 +0200

    premier test a traduire en cpp

---
+++ client.cpp
@@ -0,0 +1,56 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   client.cpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ablondel <ablondel@student.s19.be>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/29 15:32:35 by ablondel          #+#    #+#             */
+/*   Updated: 2022/06/29 19:44:27 by ablondel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/select.h>
+#include <arpa/inet.h>
+#include <poll.h>
+#include <sys/types.h>
+#include <sys/event.h>
+#include <sys/time.h>
+#include <sys/socket.h>
+#include <fcntl.h>
+#define PORT 8080
+
+int main(void)
+{
+	int client_socket = 0;
+	int ret = 0;
+	struct sockaddr_in serv_addr;
+	char *hello = "Kalashé chargé!!! Cuisine du kekra!!!";
+	char buffer[1024] = {0};
+	if ((client_socket = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+	{
+		perror("Failed to open client socket\n");
+		return -1;
+	}
+	memset(&serv_addr, '0', sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(PORT);
+	if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0)
+    {
+        printf("\nInvalid address/ Address not supported \n");
+        return -1;
+    }
+	if (connect(client_socket, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
+    {
+        printf("\nConnection Failed \n");
+        return -1;
+    }
+    send(client_socket , hello , strlen(hello) , 0 );
+    printf("MESSAGE SUCCESSFULY SENT\n");
+    ret = read( client_socket , buffer, 1024);
+    printf("%s\n",buffer );
+    return 0;
+}
---
+++ server.cpp
@@ -0,0 +1,73 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   server.cpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ablondel <ablondel@student.s19.be>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/29 15:24:09 by ablondel          #+#    #+#             */
+/*   Updated: 2022/06/29 17:48:25 by ablondel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/select.h>
+#include <arpa/inet.h>
+#include <poll.h>
+#include <sys/types.h>
+#include <sys/event.h>
+#include <sys/time.h>
+#include <sys/socket.h>
+#include <fcntl.h>
+#define PORT 8080
+#define NBHOSTS 1
+
+int main(void)
+{
+	struct sockaddr_in address;
+	int server_fd = 0;
+	int new_socket = 0;
+	int ret = 0;
+	int addrlen = sizeof(address);
+	char *hello = "KALASH! MOOLAH! KICHTA! KEKRA! De Mesnilmontant a Cambronne j'suis en roue arierre dans l'boul de ta daronne!!";
+
+	if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+	{
+		perror("Failed to open socket fd\n");
+		return -1;
+	}
+	std::cout << "Socket was openend successfully ont fd: " << server_fd << std::endl;
+	address.sin_family = AF_INET;
+	address.sin_addr.s_addr = INADDR_ANY;
+	address.sin_port = htons(PORT);
+	memset(address.sin_zero, '\0', sizeof(address.sin_zero));
+	if (bind(server_fd, (struct sockaddr*)&address, sizeof(address)) < 0)
+	{
+		perror("Failed to bind the socket\n");
+		return -1;
+	}
+	if (listen(server_fd, NBHOSTS) < 0)
+	{
+		perror("Failed to listen on socket fd\n");
+		return -1;
+	}
+	while (1)
+	{
+		std::cout << "WAITING FOR NEW CONNECTIONS..." << std::endl;
+		if ((new_socket = accept(server_fd, (struct sockaddr*)&address, (socklen_t*)&addrlen)) < 0)
+		{
+			perror("Failed to accept socket connection\n");
+			return -1;
+		}
+		char buffer[1024] = {0};
+		ret = read(new_socket, buffer, 1024);
+		buffer[ret] = 0;
+		//std::cout << "-Attempting to send: " << buffer << "-" << std::endl;
+		write(new_socket, hello, strlen(hello));
+		std::cout << "MESSAGE SUCCESSFULY SENT!" << std::endl;
+		close(new_socket);
+	}
+	return 0;
+}
