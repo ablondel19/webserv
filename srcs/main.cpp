c1c081d4f222c26f8b4d7646da4f49928aa3538e
Arnaud Blondel <ablondel@fu-r6-p4.s19.be>
2022-07-23 16:23:18 +0200

    OYEAH

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ablondel <ablondel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/21 17:51:49 by ablondel          #+#    #+#             */
-/*   Updated: 2022/07/22 20:16:42 by ablondel         ###   ########.fr       */
+/*   Updated: 2022/07/23 15:51:16 by ablondel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,24 +42,24 @@ void    close_all(std::vector<int> &sockets)
 int     set_server(std::vector<int> &sockets, std::vector<int> &ports, std::vector<struct sockaddr_in> &addrs)
 {
     int on = 1;
-	for (size_t i = 0; i < NBPORTS; i++)
+	for (size_t i = 0; i < sockets.size(); i++)
 	{
 		sockets[i] = socket(AF_INET, SOCK_STREAM, 0);
         if (sockets[i] < 0)
         {
-            std::cerr << "FAIL: while creating socket" << std::endl;
+            //std::cerr << "FAIL: while creating socket" << std::endl;
             close_all(sockets);
 			return -1;
         }
 		if (fcntl(sockets[i], F_SETFL, O_NONBLOCK) < 0)
         {
-            std::cerr << "FAIL: while adding NONBLOCK flag to the current socket" << std::endl;
+            //std::cerr << "FAIL: while adding NONBLOCK flag to the current socket" << std::endl;
             close_all(sockets);
 			return -2;
         }
 		if (setsockopt(sockets[i], SOL_SOCKET, SO_REUSEADDR, &on, sizeof(on)) < 0)
         {
-            std::cerr << "FAIL: while setting up the socket for re-use" << std::endl;
+            //std::cerr << "FAIL: while setting up the socket for re-use" << std::endl;
             close_all(sockets);
 			return -3;
         }
@@ -69,13 +69,13 @@ int     set_server(std::vector<int> &sockets, std::vector<int> &ports, std::vect
 		addrs[i].sin_port = htons(ports[i]);
 		if ((bind(sockets[i], (struct sockaddr *)&addrs[i], sizeof(addrs[i]))) < 0)
         {
-            std::cerr << "FAIL: while binding socket to the network" << std::endl;
+            //std::cerr << "FAIL: while binding socket to the network" << std::endl;
             close_all(sockets);
 			return -4;
         }
 		if ((listen(sockets[i], BACKLOG) < 0))
         {
-            std::cerr << "FAIL: while listening on the socket for incoming connections" << std::endl;
+            //std::cerr << "FAIL: while listening on the socket for incoming connections" << std::endl;
             close_all(sockets);
 			return -5;
         }
@@ -88,9 +88,6 @@ int     run_server(std::vector<int> &sockets, std::vector<int> &ports, std::vect
     std::vector<int> clients;
     struct timeval timeout;
 	unsigned char buffer[30000];
-	int current_sd;
-	int new_sd;
-	int desc_ready;
 	int end_server;
     int close_conn;
 	int rc;
@@ -105,75 +102,78 @@ int     run_server(std::vector<int> &sockets, std::vector<int> &ports, std::vect
 	fd_set read_sockets;
     FD_ZERO(&current_sockets);
     max = sockets.back();
-    for (int i = 0; i < sockets.size(); i++)
+    for (size_t i = 0; i < sockets.size(); i++)
     {
         FD_SET(sockets[i], &current_sockets);
-        log(GREEN, sockets[i], ": is connected to >> ");
-        log(GREEN, ports[i], "\n");
+        //log(GREEN, sockets[i], ": is connected to >> ");
+        //log(GREEN, ports[i], "\n");
     }
     while (end_server == false)
     {
+        FD_ZERO(&read_sockets);
         timeout.tv_usec = 0;
         timeout.tv_sec = 3 * 60;
         read_sockets = current_sockets;
         rc = select(max + 1, &read_sockets, NULL, NULL, &timeout);
         if (rc < 0)
         {
-            log(RED, rc, ": select() failed\n");
+            //log(RED, rc, ": select() failed\n");
             close_all(sockets);
             end_server = true;
             exit(1);
         }
         if (rc == 0)
         {
-            log(RED, rc, ": select timed out after 3 min\n");
+            //log(RED, rc, ": select timed out after 3 min\n");
             close_all(sockets);
             end_server = true;
             exit(1);
         }
-        for (int i = 0; i != clients.size(); i++)
+        int rd = 0;
+        int rw = 0;
+        for (size_t i = 0; i != clients.size(); i++)
         {
             close_conn = false;
 			bzero(&buffer, sizeof(buffer)); /* Clear the buffer */
-			rc = read(clients[i], buffer, 30000); /* Read data on this connection */
+			rd = read(clients[i], buffer, 30000); /* Read data on this connection */
 			//DEBUG TO SEE THE HEADER REQUEST//
 			//printf("\x1B[32m[[DATA RECEIVED]]\x1B[0m\n\n%s", buffer);
-			if (rc < 0)
+			if (rd < 0)
 			{
 				break ;
 			}
-			if (rc == 0)
+			if (rd == 0)
 			{
 				break ;
 			}
-			rc = write(clients[i], hello, strlen(hello)); /* Echo the data to the client */
-			if (rc < 0)
+			rw = write(clients[i], hello, strlen(hello)); /* Echo the data to the client */
+			if (rw < 0)
 			{
 				break ;
 			}
-			if (rc == 0)
+			if (rw == 0)
 			{
 				break ;
 			}
             close(clients[i]);
 			FD_CLR(clients[i], &current_sockets);
         }
-        for (int i = 0; i != ports.size(); i++)
+        for (size_t i = 0; i != ports.size(); i++)
         {
             if (FD_ISSET(sockets[i], &read_sockets))
             {
                 socklen_t len = sizeof(sockaddr[i]);
                 int connection = accept(sockets[i], (struct sockaddr*)&addrs[i], &len);
-                log(GREEN, connection, ": connection accepted\n");
+                //log(GREEN, connection, ": connection accepted\n");
                 if (connection < 0)
                 {
-                    log(RED, connection, ": failed to accept incoming connection\n");
+                    //log(RED, connection, ": failed to accept incoming connection\n");
                     close_all(sockets);
                     end_server = true;
                 }
                 FD_SET(connection, &current_sockets);
                 clients.push_back(connection);
-                log(GREEN, connection, ": added to fd_set\n");
+                //log(GREEN, connection, ": added to fd_set\n");
                 if (connection > max)
                     max = connection;
                 break ;
